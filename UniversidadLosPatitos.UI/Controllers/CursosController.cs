fbe1e2e9a494f029d56c262fa3d5c48c93ee7d36
Ignacio Vargas <ignacii31@gmail.com>
2025-08-04 06:20:43 -0600

    Update CursosController.cs

--- UniversidadLosPatitos.UI/Controllers/CursosController.cs
+++ UniversidadLosPatitos.UI/Controllers/CursosController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using UniversidadLosPatitos.Abstracciones.LogicaDeNegocios.Cursos.ListarCursos;
+using UniversidadLosPatitos.Abstracciones.LogicaDeNegocios.Cursos.RegistrarCursos;
 using UniversidadLosPatitos.Abstracciones.ModelosUI;
 using UniversidadLosPatitos.LogicaDeNegocios.Cursos.ListarCursos;
 
@@ -13,6 +14,7 @@ namespace UniversidadLosPatitos.UI.Controllers
     public class CursosController : Controller
     {
         private readonly IListarCursosLN listarCursosLN;
+        private readonly IRegistrarCursosLN registrarCursosLN;
 
         public CursosController() { 
             listarCursosLN = new ListarCursosLN();
@@ -22,6 +24,36 @@ namespace UniversidadLosPatitos.UI.Controllers
         {
             List<CursosDto> listaCursos = await listarCursosLN.Listar();
             return View(listaCursos);
+        }
+
+        public ActionResult RegistrarCurso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegistrarCurso(CursosDto cursoAGuardar)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int resultadoRegistroCurso = await registrarCursosLN.GuardarCurso(cursoAGuardar);
+                    if (resultadoRegistroCurso > 0)
+                    {
+                        ViewBag.Mensaje = "Curso registrado exitosamente.";
+                    }
+                    else
+                    {
+                        ViewBag.Mensaje = "Error al registrar el curso. Intente nuevamente.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Mensaje = ex.Message;
+                }
+            }
+            return RedirectToAction("ListaCursos");
         }
     }
 }
