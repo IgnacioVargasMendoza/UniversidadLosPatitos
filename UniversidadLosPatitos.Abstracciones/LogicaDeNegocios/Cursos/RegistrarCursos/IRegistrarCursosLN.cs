93511887fbe292e9819b9fcc5ed6837040d6ddd0
Ignacio Vargas <ignacii31@gmail.com>
2025-08-04 06:20:35 -0600

    Create IRegistrarCursosLN.cs

---
+++ UniversidadLosPatitos.Abstracciones/LogicaDeNegocios/Cursos/RegistrarCursos/IRegistrarCursosLN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversidadLosPatitos.Abstracciones.ModelosUI;
+
+namespace UniversidadLosPatitos.Abstracciones.LogicaDeNegocios.Cursos.RegistrarCursos
+{
+    public interface IRegistrarCursosLN
+    {
+        Task<int> GuardarCurso(CursosDto cursoAGuardar);
+    }
+}
