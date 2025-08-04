0d56831a3631fb0a46c2aea18e557ffbca1eb735
Ignacio Vargas <ignacii31@gmail.com>
2025-08-04 06:20:38 -0600

    Create RegistrarCursosAD.cs

---
+++ UniversidadLosPatitos.AccesoADatos/Cursos/RegistrarCursos/RegistrarCursosAD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversidadLosPatitos.Abstracciones.AccesoADatos.Cursos.RegistrarCursos;
+using UniversidadLosPatitos.Abstracciones.ModelosUI;
+using UniversidadLosPatitos.AccesoADatos.Contexto;
+using UniversidadLosPatitos.AccesoADatos.Modelos;
+
+namespace UniversidadLosPatitos.AccesoADatos.Cursos.RegistrarCursos
+{
+    public class RegistrarCursosAD: IRegistrarCursosAD
+    {
+        private readonly UniversidadLosPatitosContexto _contexto;
+
+        public RegistrarCursosAD()
+        {
+            _contexto = new UniversidadLosPatitosContexto();
+        }
+
+        public async Task<int> Guardar(CursosDto cursoAGuardar)
+        {
+            CursosAD cursoAGuardarEnBD = ConvertirObjeto(cursoAGuardar);
+            _contexto.Cursos.Add(cursoAGuardarEnBD);
+            int resultadoRegistroCurso = await _contexto.SaveChangesAsync();
+            return resultadoRegistroCurso;
+        }
+
+        private CursosAD ConvertirObjeto(CursosDto cursoAGuardar)
+        {
+            return new CursosAD
+            {
+                CodigoDelCurso = cursoAGuardar.CodigoDelCurso,
+                NombreCurso = cursoAGuardar.NombreCurso,
+                Carrera = cursoAGuardar.Carrera,
+                CodigoDelGrupo = cursoAGuardar.CodigoDelGrupo,
+                CantidadEstudiantesPermitidos = cursoAGuardar.CantidadEstudiantesPermitidos,
+                CantidadEstudiantesMatriculados = cursoAGuardar.CantidadEstudiantesMatriculados,
+                Modalidad = cursoAGuardar.Modalidad,
+                Profesor = cursoAGuardar.Profesor,
+                Cuatrimestre = cursoAGuardar.Cuatrimestre,
+                DiaDelCurso = cursoAGuardar.DiaDelCurso,
+                Horario = cursoAGuardar.Horario,
+            };
+        }
+    }
+ }
